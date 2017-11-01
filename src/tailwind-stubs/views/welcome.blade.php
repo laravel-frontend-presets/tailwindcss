824017ffa9958155dcd48f29f905c686dba0ef9a
Michael Dyrynda <michael@dyrynda.com.au>
2017-11-01 20:29:14 +1030

    simplify welcome view

--- src/tailwind-stubs/views/welcome.blade.php
+++ src/tailwind-stubs/views/welcome.blade.php
@@ -13,40 +13,40 @@
     <!-- Styles -->
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 </head>
-<body class="bg-grey-lighter h-screen">
-        <div class="container mx-auto h-full">
+<body class="bg-orange-lightest font-sans font-normal">
+    <div class="flex flex-col">
+        @if(Route::has('login'))
+            <div class="absolute pin-t pin-r mt-4 mr-4">
+                @auth
+                    <a href="{{ url('/home') }}" class="no-underline hover:underline text-sm font-normal text-orange-dark uppercase">Home</a>
+                @else
+                    <a href="{{ route('login') }}" class="no-underline hover:underline text-sm font-normal text-orange-dark uppercase pr-6">Login</a>
+                    <a href="{{ route('register') }}" class="no-underline hover:underline text-sm font-normal text-orange-dark uppercase">Register</a>
+                @endauth
+            </div>
+        @endif
+
+        <div class="min-h-screen flex items-center justify-center">
             <div class="flex flex-col justify-around h-full">
-                <div class="absolute pin-t pin-r">
-                    @if (Route::has('login'))
-                        <div class="pin-t pin-r mt-3 mr-4">
-                            @auth
-                                <a class="no-underline hover:underline text-blue-dark" href="{{ url('/home') }}">Home</a>
-                            @else
-                                <a class="no-underline hover:underline text-blue-dark mr-4" href="{{ url('login') }}">Login</a>
-                                <a class="no-underline hover:underline text-blue-dark" href="{{ url('register') }}">Register</a>
-                            @endauth
-                        </div>
-                    @endif
-                </div>
-                <div class="text-center">
-                    <h1 class="font-sans text-grey-darker text-5xl font-medium mb-4">
+                <div>
+                    <h1 class="text-grey-darker text-center font-hairline tracking-wide text-7xl mb-6">
                         {{ config('app.name', 'Laravel') }}
                     </h1>
                     <ul class="list-reset">
-                        <li class="inline pr-4">
-                            <a href="https://laravel.com/docs" class="no-underline hover:underline text-blue-dark" title="Documentation">Documentation</a>
+                        <li class="inline pr-8">
+                            <a href="https://laravel.com/docs" class="no-underline hover:underline text-sm font-normal text-orange-dark uppercase" title="Documentation">Documentation</a>
                         </li>
-                        <li class="inline pr-4">
-                            <a href="https://laracasts.com" class="no-underline hover:underline text-blue-dark" title="Laracasts">Laracasts</a>
+                        <li class="inline pr-8">
+                            <a href="https://laracasts.com" class="no-underline hover:underline text-sm font-normal text-orange-dark uppercase" title="Laracasts">Laracasts</a>
                         </li>
-                        <li class="inline pr-4">
-                            <a href="https://laravel-news.com" class="no-underline hover:underline text-blue-dark" title="News">News</a>
+                        <li class="inline pr-8">
+                            <a href="https://laravel-news.com" class="no-underline hover:underline text-sm font-normal text-orange-dark uppercase" title="News">News</a>
                         </li>
-                        <li class="inline pr-4">
-                            <a href="https://forge.laravel.com" class="no-underline hover:underline text-blue-dark" title="Forge">Forge</a>
+                        <li class="inline pr-8">
+                            <a href="https://forge.laravel.com" class="no-underline hover:underline text-sm font-normal text-orange-dark uppercase" title="Forge">Forge</a>
                         </li>
-                        <li class="inline pr-4">
-                            <a href="https://github.com/laravel/laravel" class="no-underline hover:underline text-blue-dark" title="GitHub">GitHub</a>
+                        <li class="inline pr-8">
+                            <a href="https://github.com/laravel/laravel" class="no-underline hover:underline text-sm font-normal text-orange-dark uppercase" title="GitHub">GitHub</a>
                         </li>
                     </ul>
                 </div>
