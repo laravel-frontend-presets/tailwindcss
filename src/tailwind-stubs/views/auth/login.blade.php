b1167d1b49e54d240a582bfe19631e3c7e2b9d71
Michael Dyrynda <michael@dyrynda.com.au>
2017-11-01 20:55:48 +1030

    add required attributes

--- src/tailwind-stubs/views/auth/login.blade.php
+++ src/tailwind-stubs/views/auth/login.blade.php
@@ -14,7 +14,7 @@
                     <div class="flex items-stretch mb-3">
                         <label for="email" class="text-right font-semibold text-grey-dark text-sm pt-2 pr-3 align-middle w-1/4">E-Mail Address</label>
                         <div class="flex flex-col w-3/4">
-                            <input id="email" type="email" class="flex-grow h-8 px-2 border rounded {{ $errors->has('email') ? 'border-red-dark' : 'border-grey-light' }}" name="email" value="{{ old('email') }}" autofocus>
+                            <input id="email" type="email" class="flex-grow h-8 px-2 border rounded {{ $errors->has('email') ? 'border-red-dark' : 'border-grey-light' }}" name="email" value="{{ old('email') }}" required autofocus>
                             {!! $errors->first('email', '<span class="text-red-dark text-sm mt-2">:message</span>') !!}
                         </div>
                     </div>
@@ -22,7 +22,7 @@
                     <div class="flex items-stretch mb-4">
                         <label for="password" class="text-right font-semibold text-grey-dark text-sm pt-2 pr-3 align-middle w-1/4">Password</label>
                         <div class="flex flex-col w-3/4">
-                            <input id="password" type="password" class="flex-grow h-8 px-2 rounded border {{ $errors->has('password') ? 'border-red-dark' : 'border-grey-light' }}" name="password">
+                            <input id="password" type="password" class="flex-grow h-8 px-2 rounded border {{ $errors->has('password') ? 'border-red-dark' : 'border-grey-light' }}" name="password" required>
                             {!! $errors->first('password', '<span class="text-red-dark text-sm mt-2">:message</span>') !!}
                         </div>
                     </div>
