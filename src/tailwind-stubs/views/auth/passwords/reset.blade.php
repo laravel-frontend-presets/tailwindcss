79350dcafaec8af4f6ce4923eb93534633fa11fa
Michael Dyrynda <michael@dyrynda.com.au>
2017-11-01 21:10:33 +1030

    update password reset form

--- src/tailwind-stubs/views/auth/passwords/reset.blade.php
+++ src/tailwind-stubs/views/auth/passwords/reset.blade.php
@@ -5,7 +5,7 @@
     <div class="flex items-center">
         <div class="md:w-1/2 md:mx-auto">
             <div class="rounded shadow">
-                <div class="font-bold text-lg bg-grey-light p-3 rounded rounded-t">
+                <div class="font-medium text-lg text-orange-darker bg-orange-lighter p-3 rounded rounded-t">
                     Reset Password
                 </div>
                 <div class="bg-white p-3 rounded rounded-b">
@@ -15,15 +15,15 @@
                         <input type="hidden" name="token" value="{{ $token }}">
 
                         <div class="flex items-stretch mb-3">
-                            <label for="email" class="font-semibold text-grey-dark text-sm pt-2 pr-3 align-middle w-1/4">E-Mail Address</label>
+                            <label for="email" class="text-right font-semibold text-grey-dark text-sm pt-2 pr-3 align-middle w-1/4">E-Mail Address</label>
                             <div class="flex flex-col w-3/4">
-                                <input id="email" type="email" class="flex-grow h-8 px-2 border rounded {{ $errors->has('email') ? 'border-red-dark' : 'border-grey-light' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="email" type="email" class="flex-grow h-8 px-2 border rounded {{ $errors->has('email') ? 'border-red-dark' : 'border-grey-light' }}" name="email" value="{{ $email or old('email') }}" required autofocus>
                                 {!! $errors->first('email', '<span class="text-red-dark text-sm mt-2">:message</span>') !!}
                             </div>
                         </div>
 
                         <div class="flex items-stretch mb-3">
-                            <label for="password" class="font-semibold text-grey-dark text-sm pt-2 pr-3 align-middle w-1/4">Password</label>
+                            <label for="password" class="text-right font-semibold text-grey-dark text-sm pt-2 pr-3 align-middle w-1/4">Password</label>
                             <div class="flex flex-col w-3/4">
                                 <input id="password" type="password" class="flex-grow h-8 px-2 rounded border {{ $errors->has('password') ? 'border-red-dark' : 'border-grey-light' }}" name="password" required>
                                 {!! $errors->first('password', '<span class="text-red-dark text-sm mt-2">:message</span>') !!}
@@ -31,16 +31,16 @@
                         </div>
 
                         <div class="flex items-stretch mb-3">
-                            <label for="password_confirmation" class="font-semibold text-grey-dark text-sm pt-2 pr-3 align-middle w-1/4">Confirm Password</label>
+                            <label for="password_confirmation" class="text-right font-semibold text-grey-dark text-sm pt-2 pr-3 align-middle w-1/4">Confirm Password</label>
                             <div class="flex flex-col w-3/4">
-                                <input id="password_confirmation" type="password_confirmation" class="flex-grow h-8 px-2 rounded border {{ $errors->has('password_confirmation') ? 'border-red-dark' : 'border-grey-light' }}" name="password" required>
+                                <input id="password_confirmation" type="password" class="flex-grow h-8 px-2 rounded border {{ $errors->has('password_confirmation') ? 'border-red-dark' : 'border-grey-light' }}" name="password_confirmation" required>
                                 {!! $errors->first('password_confirmation', '<span class="text-red-dark text-sm mt-2">:message</span>') !!}
                             </div>
                         </div>
 
                         <div class="flex">
                             <div class="w-3/4 ml-auto">
-                                <button type="submit" class="bg-blue hover:bg-blue-dark text-white text-sm font-sembiold py-2 px-4 rounded mr-3">
+                                <button type="submit" class="bg-orange hover:bg-orange-dark text-white text-sm font-sembiold py-2 px-4 rounded mr-3">
                                     Reset Password
                                 </button>
                             </div>
