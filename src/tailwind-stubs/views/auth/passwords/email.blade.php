09620fcced01177437b6075ea2dab5c73ecf4c76
Michael Dyrynda <michael@dyrynda.com.au>
2017-11-01 21:10:24 +1030

    updated password request view

--- src/tailwind-stubs/views/auth/passwords/email.blade.php
+++ src/tailwind-stubs/views/auth/passwords/email.blade.php
@@ -1,37 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container mx-auto">
-    <div class="flex items-center">
-        <div class="md:w-1/2 md:mx-auto">
-            <div class="rounded shadow">
-                <div class="font-bold text-lg bg-grey-light p-3 rounded rounded-t">
-                    Reset Password
-                </div>
-                <div class="bg-white p-3 rounded rounded-b">
-                    <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
+<div class="flex items-center">
+    <div class="md:w-1/2 md:mx-auto">
+        <div class="rounded shadow">
+            <div class="font-medium text-lg text-orange-darker bg-orange-lighter p-3 rounded rounded-t">
+                Reset Password
+            </div>
+            <div class="bg-white p-3 rounded rounded-b">
+                @if (session('status'))
+                    <div class="bg-green-lightest border border-green-light text-green-dark text-sm px-4 py-3 rounded mb-4">
+                        {{ session('status') }}
+                    </div>
+                @endif
 
-                        <div class="flex items-stretch mb-3">
-                            <label for="email" class="font-semibold text-grey-dark text-sm pt-2 pr-3 align-middle w-1/4">E-Mail Address</label>
-                            <div class="flex flex-col w-3/4">
-                                <input id="email" type="email" class="flex-grow h-8 px-2 border rounded {{ $errors->has('email') ? 'border-red-dark' : 'border-grey-light' }}" name="email" value="{{ old('email') }}" required autofocus>
-                                {!! $errors->first('email', '<span class="text-red-dark text-sm mt-2">:message</span>') !!}
-                            </div>
-                        </div>
+                <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
+                    {{ csrf_field() }}
 
-                        <div class="flex">
-                            <div class="w-3/4 ml-auto">
-                                <button type="submit" class="bg-blue hover:bg-blue-dark text-white text-sm font-sembiold py-2 px-4 rounded mr-3">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
+                    <div class="flex items-stretch mb-3">
+                        <label for="email" class="text-right font-semibold text-grey-dark text-sm pt-2 pr-3 align-middle w-1/4">E-Mail Address</label>
+                        <div class="flex flex-col w-3/4">
+                            <input id="email" type="email" class="flex-grow h-8 px-2 border rounded {{ $errors->has('email') ? 'border-red-dark' : 'border-grey-light' }}" name="email" value="{{ old('email') }}" required autofocus>
+                            {!! $errors->first('email', '<span class="text-red-dark text-sm mt-2">:message</span>') !!}
                         </div>
-                    </form>
-                </div>
+                    </div>
+
+                    <div class="flex">
+                        <div class="w-3/4 ml-auto">
+                            <button type="submit" class="bg-orange hover:bg-orange-dark text-white text-sm font-sembiold py-2 px-4 rounded mr-3">
+                                Send Password Reset Link
+                            </button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
 </div>
 @endsection
-
