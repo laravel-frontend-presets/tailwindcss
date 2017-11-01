b12403aa1c9be65840cab6f2c4ee5f52e6608474
Michael Dyrynda <michael@dyrynda.com.au>
2017-11-01 21:03:16 +1030

    update home view

--- src/tailwind-stubs/views/home.blade.php
+++ src/tailwind-stubs/views/home.blade.php
@@ -2,24 +2,22 @@
 
 @section('content')
 
-<div class="container mx-auto">
-    <div class="flex items-center">
-        <div class="md:w-1/2 md:mx-auto">
-            <div class="rounded shadow">
-                <div class="font-bold text-lg bg-grey-light p-3 rounded rounded-t">
-                    Dashboard
-                </div>
-                <div class="bg-white p-3 rounded rounded-b">
-                    @if (session('status'))
-                        <div class="bg-green-lightest border border-green-light text-green-dark text-sm px-4 py-3 rounded mb-4">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<div class="flex items-center">
+    <div class="md:w-1/2 md:mx-auto">
+        <div class="rounded shadow">
+            <div class="font-medium text-lg text-orange-darker bg-orange-lighter p-3 rounded rounded-t">
+                Dashboard
+            </div>
+            <div class="bg-white p-3 rounded rounded-b">
+                @if (session('status'))
+                    <div class="bg-green-lightest border border-green-light text-green-dark text-sm px-4 py-3 rounded mb-4">
+                        {{ session('status', 'hi') }}
+                    </div>
+                @endif
 
-                    <p class="text-grey-dark text-base">
-                        You are logged in!
-                    </p>
-                </div>
+                <p class="text-grey-dark text-sm">
+                    You are logged in!
+                </p>
             </div>
         </div>
     </div>
