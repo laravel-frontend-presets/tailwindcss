7bf5bb267dee43b2aa666cb3fb0faa5f6c41a490
Laurence Ioannou <github@theshiftexchange.com>
2017-12-21 06:23:45 +0000

    Update webpack.mix.js

--- src/tailwindcss-stubs/webpack.mix.js
+++ src/tailwindcss-stubs/webpack.mix.js
@@ -1,5 +1,13 @@
 let mix = require('laravel-mix');
 var tailwindcss = require('tailwindcss');
+let glob = require("glob-all");
+let PurgecssPlugin = require("purgecss-webpack-plugin");
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g) || [];
+  }
+}
 
 /*
  |--------------------------------------------------------------------------
@@ -31,3 +39,31 @@ mix.js('resources/assets/js/app.js', 'public/js')
 //       processCssUrls: false,
 //       postCss: [ tailwindcss('tailwind.js') ],
 //    });
+
+// Only run PurgeCSS during production builds for faster development builds
+// and so you still have the full set of utilities available during
+// development.
+if (mix.inProduction()) {
+  mix.webpackConfig({
+    plugins: [
+      new PurgecssPlugin({
+
+        // Specify the locations of any files you want to scan for class names.
+        paths: glob.sync([
+          path.join(__dirname, "resources/views/**/*.blade.php"),
+          path.join(__dirname, "resources/assets/js/**/*.vue")
+        ]),
+        extractors: [
+          {
+            extractor: TailwindExtractor,
+
+            // Specify the file extensions to include when scanning for
+            // class names.
+            extensions: ["html", "js", "php", "vue"]
+          }
+        ]
+      })
+    ]
+  });
+}
+
