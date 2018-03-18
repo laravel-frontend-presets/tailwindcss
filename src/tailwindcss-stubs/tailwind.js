d2539205667cf2326a61035df64b6625e6b951cb
Michael Dyrynda <michael@dyrynda.com.au>
2018-03-19 09:59:59 +1030

    update path to container plugin - fixes #24

--- src/tailwindcss-stubs/tailwind.js
+++ src/tailwindcss-stubs/tailwind.js
@@ -891,7 +891,7 @@ module.exports = {
   */
 
   plugins: [
-    require("./plugins/container")({
+    require("tailwindcss/plugins/container")({
       // center: true,
       // padding: '1rem',
     })
