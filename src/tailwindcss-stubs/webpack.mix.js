5cbe3d53e7efac99d6d2f9055f38dc0fe5003494
Michael Dyrynda <michael@dyrynda.com.au>
2018-05-09 14:44:10 +0930

    Remove comments for LESS/SASS preprocessing.
    The `laravel-mix-tailwind` plugin handles this for us, so there's
    nothing exceptional you need to do if you want to use `mix.less()` or
    `mix.sass()` any more.

--- src/tailwindcss-stubs/webpack.mix.js
+++ src/tailwindcss-stubs/webpack.mix.js
@@ -22,18 +22,3 @@ mix.js("resources/assets/js/app.js", "public/js")
 if (mix.inProduction()) {
   mix.version();
 }
-
-// If you want to use LESS for your preprocessing
-// mix.less('resources/assets/less/main.less', 'public/css')
-//   .options({
-//     postCss: [
-//       tailwindcss('./tailwind.js'),
-//     ]
-//   })
-
-// If you want to use SASS for preprocessing
-// mix.sass('resources/assets/sass/app.scss', 'public/css')
-//    .options({
-//       processCssUrls: false,
-//       postCss: [ tailwindcss('tailwind.js') ],
-//    });
