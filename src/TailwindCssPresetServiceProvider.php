397426f8f8be8da028550bf839c8a428744d0e84
TJ Miller <sixlive@users.noreply.github.com>
2018-08-11 06:27:23 -0400

    Remove duplicated instruction
    Should only need to run `npm run dev` once.

--- src/TailwindCssPresetServiceProvider.php
+++ src/TailwindCssPresetServiceProvider.php
@@ -13,14 +13,14 @@ class TailwindCssPresetServiceProvider extends ServiceProvider
             TailwindCssPreset::install();
 
             $command->info('Tailwind CSS scaffolding installed successfully.');
-            $command->info('Please run "npm install && npm run dev && npm run dev" to compile your fresh scaffolding.');
+            $command->info('Please run "npm install && npm run dev" to compile your fresh scaffolding.');
         });
 
         PresetCommand::macro('tailwindcss-auth', function ($command) {
             TailwindCssPreset::installAuth();
 
             $command->info('Tailwind CSS scaffolding with auth views installed successfully.');
-            $command->info('Please run "npm install && npm run dev && npm run dev" to compile your fresh scaffolding.');
+            $command->info('Please run "npm install && npm run dev" to compile your fresh scaffolding.');
         });
     }
 }
