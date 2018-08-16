130edeb41ff3ca0d46e17a4bb059a50bbb1d5721
Randy Rankin <rrrankin@gmail.com>
2018-08-16 10:56:20 -0500

    Change asset() to mix() in app.blade.php

--- src/tailwindcss-stubs/resources/views/layouts/app.blade.php
+++ src/tailwindcss-stubs/resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body class="bg-teal-lightest h-screen antialiased">
     <div id="app">
@@ -47,6 +47,6 @@
     </div>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
