c60691f2dad2da4ded644e3af0dab9aef8a89735
Michael Dyrynda <michael@dyrynda.com.au>
2018-09-07 14:26:24 +0930

    remove X-UA-Compatible per laravel/framework#25442

--- src/tailwindcss-stubs/resources/views/layouts/app.blade.php
+++ src/tailwindcss-stubs/resources/views/layouts/app.blade.php
@@ -1,8 +1,7 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
