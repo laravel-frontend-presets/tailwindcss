5e4ca511f2f8b8c3187444902ab2ea0deb929a6b
Eddie Palmans <epalmans@gmail.com>
2019-01-21 15:00:40 +0100

    Use mix() instead of asset() for css on welcome page

--- src/tailwindcss-stubs/resources/views/welcome.blade.php
+++ src/tailwindcss-stubs/resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body class="bg-teal-lightest font-sans font-normal antialiased">
     <div class="flex flex-col">
