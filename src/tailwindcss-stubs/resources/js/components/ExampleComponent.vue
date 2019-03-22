2c85d59041af25707c277a27c45cfd5cfe68c307
Roy Verrips <roy@verrips.org>
2019-03-22 01:36:25 -0500

    Update example Vue Component for Tailwind v1

--- src/tailwindcss-stubs/resources/js/components/ExampleComponent.vue
+++ src/tailwindcss-stubs/resources/js/components/ExampleComponent.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
-        <div class="font-semibold bg-grey-lightest text-grey-darkest py-3 px-6 mb-0 shadow-inner">
+        <div class="font-semibold bg-gray-100 text-gray-700 py-3 px-6 mb-0 shadow-inner">
                 Example Component</div>
                 <div class="w-full p-6">
-                    <p class="text-grey-darkest">
+                    <p class="text-gray-700">
                         I'm an example component.
                     </p>
                 </div>
