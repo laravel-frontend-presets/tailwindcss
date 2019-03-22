337ab543c0436001cd76d04cca8e545b3c624fd0
Roy Verrips <roy@verrips.org>
2019-03-22 00:23:49 -0500

    finetune welcome.blade.php

--- src/tailwindcss-stubs/resources/views/welcome.blade.php
+++ src/tailwindcss-stubs/resources/views/welcome.blade.php
@@ -31,7 +31,7 @@
     <div class="min-h-screen flex items-center justify-center">
         <div class="flex flex-col justify-around h-full">
             <div>
-                <h1 class="text-gray-800 text-center font-thin tracking-wider text-5xl mb-6">
+                <h1 class="text-gray-600 text-center font-light tracking-wider text-5xl mb-6">
                     {{ config('app.name', 'Laravel') }}
                 </h1>
                 <ul class="list-reset">
