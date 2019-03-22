9867a47cdc592efbf7ebf3fd6cd37153061d6952
Roy Verrips <roy@verrips.org>
2019-03-22 01:03:04 -0500

    update home to tailwind v1

--- src/tailwindcss-stubs/resources/views/home.blade.php
+++ src/tailwindcss-stubs/resources/views/home.blade.php
@@ -5,19 +5,19 @@
         <div class="md:w-1/2 md:mx-auto">
 
             @if (session('status'))
-                <div class="text-sm border border-t-8 rounded text-green-darker border-green-dark bg-green-lightest px-3 py-4 mb-4" role="alert">
+                <div class="text-sm border border-t-8 rounded text-green-700 border-green-600 bg-green-100 px-3 py-4 mb-4" role="alert">
                     {{ session('status') }}
                 </div>
             @endif
 
             <div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
 
-                <div class="font-semibold bg-grey-lightest text-grey-darkest py-3 px-6 mb-0 shadow-inner">
+                <div class="font-semibold bg-gray-200 text-gray-700 py-3 px-6 mb-0 shadow-inner">
                     Dashboard
                 </div>
 
                 <div class="w-full p-6">
-                    <p class="text-grey-darkest">
+                    <p class="text-gray-700">
                         You are logged in!
                     </p>
                 </div>
