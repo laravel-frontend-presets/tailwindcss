1774d827c3b9e35cdf17e35b96ddae8e59a16b6c
Roy Verrips <roy@verrips.org>
2019-03-23 19:17:25 -0500

    remove doubled up font-bold - thanks to @imliam

--- src/tailwindcss-stubs/resources/views/auth/passwords/reset.blade.php
+++ src/tailwindcss-stubs/resources/views/auth/passwords/reset.blade.php
@@ -52,7 +52,7 @@
                         </div>
 
                         <div class="flex flex-wrap">
-                            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-gray-100 font-bold font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-gray-100 font-bold  py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                                 {{ __('Reset Password') }}
                             </button>
                         </div>
