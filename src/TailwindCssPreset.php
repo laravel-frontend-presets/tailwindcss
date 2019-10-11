acac5ae9687a941bb077b18d22749b67f58aae86
Travis Northcutt <travis@travisnorthcutt.com>
2019-10-11 08:22:57 -0600

    Don't require Vue or vue-template-compiler
    These two package aren't used in this preset, so no need to require them.
    
    Closes #67

--- src/TailwindCssPreset.php
+++ src/TailwindCssPreset.php
@@ -31,8 +31,6 @@ class TailwindCssPreset extends Preset
             'laravel-mix-purgecss' => '^4.1',
             'laravel-mix-tailwind' => '^0.1.0',
             'tailwindcss' => '^1.0',
-            'vue' => '^2.5.17',
-            'vue-template-compiler' => '^2.6.4',
         ], Arr::except($packages, [
             'bootstrap',
             'bootstrap-sass',
