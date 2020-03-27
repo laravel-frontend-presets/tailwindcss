e2328af7cc926d55caf8a8576c649766061e1ced
AndrewP <spargo32@gmail.com>
2020-03-27 09:55:28 +0300

    Scripts to the head

--- src/tailwindcss-stubs/resources/views/layouts/app.blade.php
+++ src/tailwindcss-stubs/resources/views/layouts/app.blade.php
@@ -9,6 +9,9 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
@@ -46,8 +49,5 @@
 
         @yield('content')
     </div>
-
-    <!-- Scripts -->
-    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
