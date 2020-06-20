eb02d98f54593d6f4e70751b6d77da14e7a52927
Alessio <alessio.vertemati@gmail.com>
2020-06-20 14:31:34 +0200

    Make postCSS configuration explicit

--- src/tailwindcss-stubs/webpack.mix.js
+++ src/tailwindcss-stubs/webpack.mix.js
@@ -1,7 +1,5 @@
 const mix = require('laravel-mix');
 
-require('laravel-mix-tailwind');
-
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -14,8 +12,12 @@ require('laravel-mix-tailwind');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .postCss('resources/css/app.css', 'public/css')
-   .tailwind('./tailwind.config.js');
+   .postCss('resources/css/app.css', 'public/css', [
+      require('postcss-import'),
+      require('tailwindcss'),
+      require('postcss-nested'),
+      require('autoprefixer'),
+  ]);
 
 if (mix.inProduction()) {
   mix
