7ad53e5a22481d26ed50d28e590195ab8e48f57c
Michael Dyrynda <michael@dyrynda.com.au>
2020-08-09 21:27:50 +0930

    cs

--- src/tailwindcss-stubs/webpack.mix.js
+++ src/tailwindcss-stubs/webpack.mix.js
@@ -11,15 +11,16 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .postCss('resources/css/app.css', 'public/css', [
-      require('postcss-import'),
-      require('tailwindcss'),
-      require('postcss-nested'),
-      require('autoprefixer'),
+mix
+  .js('resources/js/app.js', 'public/js')
+  .postCss('resources/css/app.css', 'public/css', [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('postcss-nested'),
+    require('autoprefixer'),
   ]);
 
 if (mix.inProduction()) {
   mix
-   .version();
+    .version();
 }
