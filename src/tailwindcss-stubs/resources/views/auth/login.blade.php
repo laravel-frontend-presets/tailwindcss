4986a01e92fd123a933d4407f806ca94c7bc2899
Marvin Quezon <quezon.marvin@gmail.com>
2020-12-19 13:35:46 +0800

    Fixes bottom margin on login screen if registration is disabled.

--- src/tailwindcss-stubs/resources/views/auth/login.blade.php
+++ src/tailwindcss-stubs/resources/views/auth/login.blade.php
@@ -62,7 +62,7 @@
 
                     <div class="flex flex-wrap">
                         <button type="submit"
-                        class="w-full select-none font-bold whitespace-no-wrap p-3 rounded-lg text-base leading-normal no-underline text-gray-100 bg-blue-500 hover:bg-blue-700 sm:py-4">
+                        class="w-full select-none font-bold whitespace-no-wrap p-3 rounded-lg text-base leading-normal no-underline text-gray-100 bg-blue-500 hover:bg-blue-700 sm:py-4 {{ ! Route::has('register') ? 'mb-8' : '' }}">
                             {{ __('Login') }}
                         </button>
 
