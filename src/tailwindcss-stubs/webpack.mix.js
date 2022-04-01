2d0b9a49e1ef6f149e8b1a0130c6582265f69e3c
Michael Dyrynda <michael@dyrynda.com.au>
2022-04-01 16:51:08 +1030

    update for Laravel 9 - closes #133

--- src/tailwindcss-stubs/webpack.mix.js
+++ src/tailwindcss-stubs/webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -12,15 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix
-  .js('resources/js/app.js', 'public/js')
-  .postCss('resources/css/app.css', 'public/css', [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('postcss-nested'),
-    require('autoprefixer'),
-  ]);
+  .js("resources/js/app.js", "public/js")
+  .postCss("resources/css/app.css", "public/css", [require("tailwindcss")]);
 
 if (mix.inProduction()) {
-  mix
-    .version();
+  mix.version();
 }
